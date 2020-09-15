9116779df6c3cb07ea38f5b3783978d062646cb3
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2020-09-15 14:53:00 +1200

    Add npm script to initialise database

--- database/initialise_database.sh
+++ database/initialise_database.sh
@@ -1,6 +1,6 @@
 echo "Creating database..."
 
-# psql -U postgres -f ./database/createDatabase.sql
+psql -U postgres -f ./database/createDatabase.sql
 
 echo "Building schema..."
 
@@ -8,3 +8,7 @@ for file in ./database/buildSchema/*; do
     echo $file
     psql -U postgres -d tmf_app_manager -f $file
 done
+
+echo "Inserting data..."
+
+exec node ./database/insertData.js
