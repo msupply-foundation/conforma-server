52d0fcc405e27ceaf0a5970cafeef5add4c2e4e5
nmadruga <katinachovansky@gmail.com>
2020-09-17 16:39:56 +1200

    Additions to the schema - template stage

--- database/buildSchema/07_template.sql
+++ database/buildSchema/07_template.sql
@@ -1,5 +1,7 @@
 -- (application) template table
 
+CREATE TYPE public.template_status AS ENUM ('Draft', 'Available', 'Disabled');
+
 CREATE TABLE public.template (
     id serial primary key,
     version_id integer references public.template_version,
