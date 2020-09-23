c1339a484a1eeb5df1319d2e3e487117d6debe9a
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2020-09-23 13:07:27 +1200

    Update database/buildSchema/07_template.sql

--- database/buildSchema/07_template.sql
+++ database/buildSchema/07_template.sql
@@ -5,7 +5,7 @@ CREATE TYPE public.template_status AS ENUM ('Draft', 'Available', 'Disabled');
 CREATE TABLE public.template (
     id serial primary key,
     name varchar,
-    code varchar,
+code varchar NOT NULL,
     status public.template_status,
     version_timestamp timestamp   
 );
