f379495e290257ebcde0d72d49cf36b3a3cfc534
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2020-09-24 10:59:50 +1200

    Move postgraphile options to .postgraphilerc.js

--- .postgraphilerc.js
+++ .postgraphilerc.js
@@ -1,10 +1,14 @@
 module.exports = {
+  dynamicJson: true,
   options: {
     appendPlugins: [
       '@graphile-contrib/pg-simplify-inflector',
       'postgraphile-plugin-nested-mutations',
       'postgraphile-plugin-connection-filter',
     ],
+    graphiql: '/graphiql',
+    enhanceGraphiql: true,
+    cors: true,
     watch: true,
     graphileBuildOptions: {
       connectionFilterRelations: true,
--- utils/postgraphile_launch.sh
+++ utils/postgraphile_launch.sh
@@ -1,4 +1,3 @@
 #TO-DO: Replace string with variable for DB name
 
-npx postgraphile -c "postgres://postgres@localhost/tmf_app_manager" -C postgres --watch --enhance-graphiql --dynamic-json --cors --export-schema-graphql "database/schema.graphql"
-# --append-plugins @graphile-contrib/pg-simplify-inflector,postgraphile-plugin-nested-mutations,postgraphile-plugin-connection-filter
+npx postgraphile -c "postgres://postgres@localhost/tmf_app_manager" -C postgres
