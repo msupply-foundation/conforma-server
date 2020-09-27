9c7c93e1d1470b36de4eaba15cf09e9a96cc424a
nmadruga <katinachovansky@gmail.com>
2020-09-28 12:25:09 +1300

    Update field link to be same as table

--- database/buildSchema/29_review_section_join.sql
+++ database/buildSchema/29_review_section_join.sql
@@ -3,7 +3,7 @@
 CREATE TABLE public.review_section_join (
 	id serial primary key,
 	review_id integer references public.review(id),
-	section_assignment_id integer references public.review_section_assignment(id),
+	review_section_assignment_id integer references public.review_section_assignment(id),
 	review_section_id integer references public.review_section(id),
 	send_to_applicant boolean
 );
