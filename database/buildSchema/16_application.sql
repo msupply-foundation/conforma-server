4e715ff2dab40f12d811b8961a22334534054c39
nmadruga <nmadruga@users.noreply.github.com>
2020-10-20 10:55:19 +1300

    Update database/buildSchema/16_application.sql
    Co-authored-by: Carl Smith <5456533+CarlosNZ@users.noreply.github.com>

--- database/buildSchema/16_application.sql
+++ database/buildSchema/16_application.sql
@@ -8,7 +8,7 @@ CREATE TABLE public.application (
     id serial primary key,
     template_id integer references public.template(id),
     user_id integer references public.user(id),
-    serial varchar,
+serial varchar UNIQUE,
     name varchar,
     outcome public.application_outcome,
     is_active bool,
