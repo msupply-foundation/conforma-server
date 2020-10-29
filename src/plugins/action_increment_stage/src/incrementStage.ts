c44a964102dc3914e055b54ffc81be25468d4fbc
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2020-10-29 14:14:37 +1300

    New plugin skeleton

---
+++ src/plugins/action_increment_stage/src/incrementStage.ts
@@ -0,0 +1,26 @@
+module.exports['changeOutcome'] = async function (parameters: any, DBConnect: any) {
+  const { application_id, newOutcome } = parameters
+  try {
+    console.log(`\nUpdating application: ${newOutcome}`)
+    const success = await DBConnect.setApplicationOutcome(application_id, newOutcome)
+    if (success)
+      return {
+        status: 'Success',
+        error_log: '',
+        output: {
+          applicationId: application_id,
+          newOutcome,
+        },
+      }
+    else
+      return {
+        status: 'Fail',
+        error_log: 'There was a problem updating the application.',
+      }
+  } catch (error) {
+    return {
+      status: 'Fail',
+      error_log: 'There was a problem updating the application.',
+    }
+  }
+}
