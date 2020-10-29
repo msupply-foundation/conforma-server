f5dc4fb75a2fbae18bb74ded218b3f0c8d9a3f4a
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2020-10-30 12:36:35 +1300

    Trigger and Function to ensure only latest stage_history is_current

--- database/buildSchema/18_application_stage_history.sql
+++ database/buildSchema/18_application_stage_history.sql
@@ -7,3 +7,22 @@ CREATE TABLE public.application_stage_history (
     time_created timestamp,
     is_current bool DEFAULT true
 );
+
+
+-- FUNCTION to set `is_current` to false on all other stage_histories of current application
+CREATE OR REPLACE FUNCTION public.stage_is_current_update()
+RETURNS trigger as $application_stage_history_event$
+BEGIN
+	UPDATE public.application_stage_history SET is_current = false
+	WHERE application_id = NEW.application_id AND id<>NEW.id;
+RETURN NULL;
+END;
+$application_stage_history_event$
+LANGUAGE plpgsql;
+
+
+--TRIGGER to run above function when outcome is updated
+CREATE TRIGGER application_stage_history_trigger AFTER INSERT OR UPDATE OF is_current ON public.application_stage_history
+FOR EACH ROW
+WHEN (NEW.is_current = true)
+EXECUTE FUNCTION public.stage_is_current_update()
