e2ab0349d3afb4346800a9785121275d3733103b
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2020-11-04 21:14:21 +1300

    Simplify changeStatus logic as per PR review suggestion

--- src/plugins/action_change_status/src/changeStatus.ts
+++ src/plugins/action_change_status/src/changeStatus.ts
@@ -24,57 +24,43 @@ module.exports['changeStatus'] = async function (
   try {
     const currentStatus = await DBConnect.getCurrentStatusHistory(applicationId)
 
-    switch (currentStatus?.status) {
-      case undefined:
-        // Create initial status
-        const currentStageHistoryId = await DBConnect.getCurrentStageHistory(applicationId)
-        if (!currentStageHistoryId) {
-          console.log(
-            "No stage defined for this Application. Can't create a status_history record."
-          )
-          returnObject.status = 'Fail'
-          returnObject.error_log = 'Missing stage_history for Application'
-        } else {
-          const result = await DBConnect.addNewStatusHistory(currentStageHistoryId.id, newStatus)
-          if (result.id) {
-            returnObject.status = 'Success'
-            returnObject.output = { status: newStatus, statusId: result.id }
-            console.log(`New status_history created: ${newStatus}`)
-          } else {
-            returnObject.status = 'Fail'
-            returnObject.error_log = "Couldn't create new application_status_history"
-          }
-        }
-        break
-      case newStatus:
-        // Do nothing
-        console.log(
-          `WARNING: Application ${applicationId} already has status: ${newStatus}. No changes were made.`
-        )
-        returnObject.status = 'Success'
-        returnObject.error_log = 'Status not changed'
-        returnObject.output = { status: newStatus, statusId: currentStatus.id }
-        break
-      default:
-        // Create a new application_status_history record
-        const result = await DBConnect.addNewStatusHistory(
-          currentStatus.application_stage_history_id,
-          newStatus
-        )
-        if (result.id) {
-          returnObject.status = 'Success'
-          returnObject.output = { status: newStatus, statusId: result.id }
-          console.log(`Application status changed to: ${newStatus}`)
-        } else {
-          returnObject.status = 'Fail'
-          returnObject.error_log = "Couldn't create new application_status_history"
-          console.log("Couldn't create new application_status_history")
-        }
+    if (currentStatus?.status === newStatus) {
+      // Do nothing
+      console.log(
+        `WARNING: Application ${applicationId} already has status: ${newStatus}. No changes were made.`
+      )
+      returnObject.status = 'Success'
+      returnObject.error_log = 'Status not changed'
+      returnObject.output = { status: newStatus, statusId: currentStatus.id }
+      return returnObject
+    }
+
+    // Get current stage_history_id if not already known from status_history
+    let currentStageHistoryId
+    if (!currentStatus?.status) {
+      const result = await DBConnect.getCurrentStageHistory(applicationId)
+      if (!result) {
+        console.log("No stage defined for this Application. Can't create a status_history record.")
+        returnObject.status = 'Fail'
+        returnObject.error_log = 'Missing stage_history for Application'
+        return returnObject
+      } else currentStageHistoryId = result.id
+    } else currentStageHistoryId = currentStatus.application_stage_history_id
+
+    // Create a new application_status_history record
+    const result = await DBConnect.addNewStatusHistory(currentStageHistoryId, newStatus)
+    if (result.id) {
+      returnObject.status = 'Success'
+      returnObject.output = { status: newStatus, statusId: result.id }
+      console.log(`New status_history created: ${newStatus}`)
+    } else {
+      returnObject.status = 'Fail'
+      returnObject.error_log = "Couldn't create new application_status_history"
+      return returnObject
     }
 
     // Create output object and return
-    if (returnObject.status !== 'Fail')
-      returnObject.output = { ...returnObject.output, applicationId }
+    returnObject.output = { ...returnObject.output, applicationId }
     return returnObject
   } catch (err) {
     console.log('Unable to change Status')
