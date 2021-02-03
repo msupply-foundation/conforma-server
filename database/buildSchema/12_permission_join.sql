333aba18ed2d1434e0d633b8e383242623993b23
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2021-02-03 16:02:43 +1300

    Fix reversion of permission_join

--- database/buildSchema/12_permission_join.sql
+++ database/buildSchema/12_permission_join.sql
@@ -3,6 +3,6 @@
 CREATE TABLE public.permission_join (
     id serial primary key,
     user_id integer references public.user(id),
-    user_organisation_id integer references public.user_organisation(id),
+    organisation_id integer references public.organisation(id),
     permission_name_id integer references public.permission_name(id)
 );
