9404a5d83793b1beed70ad4b446414eda05d4508
andreievg <andrei.evguenov@gmail.com>
2021-03-19 11:24:08 +1300

    Add reviewer list and use function for application list

---
+++ database/buildSchema/37_review_list.sql
@@ -0,0 +1,37 @@
+-- Aggregated VIEW method of all data review data, use in application_list for application list page
+CREATE FUNCTION review_list (reviewerid int)
+    RETURNS TABLE (
+        application_id int,
+        review_available_for_self_assignment_count bigint,
+        review_assigned_count bigint,
+        review_assigned_not_started_count bigint,
+        review_draft_count bigint,
+        review_submitted_count bigint,
+        review_change_request_count bigint,
+        review_pending_count bigint
+    )
+    AS $$
+    SELECT
+        review_assignment.application_id AS application_id,
+        COUNT(*) FILTER (WHERE review_assignment.status = 'Available for self-assignment') AS review_available_for_self_assignment_count,
+        COUNT(*) FILTER (WHERE review_assignment.status = 'Assigned') AS review_assigned_count,
+        COUNT(*) FILTER (WHERE review_assignment.status = 'Assigned'
+            AND review.id IS NULL) AS review_assigned_not_started_count,
+        COUNT(*) FILTER (WHERE review_status_history.status = 'Draft') AS review_draft_count,
+        COUNT(*) FILTER (WHERE review_status_history.status = 'Submitted') AS review_submitted_count,
+        COUNT(*) FILTER (WHERE review_status_history.status = 'Changes Requested') AS review_change_request_count,
+        COUNT(*) FILTER (WHERE review_status_history.status = 'Changes Requested') AS review_pending_count
+    FROM
+        review_assignment
+    LEFT JOIN review ON review.review_assignment_id = review_assignment.id
+    LEFT JOIN review_status_history ON (review_status_history.review_id = review.id
+            AND is_current = TRUE)
+WHERE
+    review_assignment.reviewer_id = $1
+GROUP BY
+    review_assignment.application_id;
+
+$$
+LANGUAGE sql
+STABLE;
+
