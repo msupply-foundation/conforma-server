a70e1b38705a18af34704c3c3a3e6e7272cd1e1d
andreievg <andrei.evguenov@gmail.com>
2021-03-19 11:24:08 +1300

    Add reviewer list and use function for application list

---
+++ database/buildSchema/38_application_list_view.sql
@@ -0,0 +1,68 @@
+-- Aggregated VIEW method of all data required for application list page
+-- Requires an empty table as setof return and smart comment to make orderBy work (https://github.com/graphile/graphile-engine/pull/378)
+CREATE TABLE application_list_shape (
+    id int,
+    "serial" varchar,
+    "name" varchar,
+    template_code varchar,
+    template_name varchar,
+    applicant_username varchar,
+    applicant_first_name varchar,
+    applicant_last_name varchar,
+    applicant varchar,
+    org_name varchar,
+    stage varchar,
+    "status" public.application_status,
+    outcome public.application_outcome,
+    last_active_date timestamptz,
+    is_fully_assigned_level_1 boolean,
+    review_available_for_self_assignment_count bigint,
+    review_assigned_count bigint,
+    review_assigned_not_started_count bigint,
+    review_draft_count bigint,
+    review_submitted_count bigint,
+    review_change_request_count bigint,
+    review_pending_count bigint
+);
+
+CREATE FUNCTION application_list (reviewerid int)
+    RETURNS SETOF application_list_shape
+    AS $$
+    SELECT
+        app.id,
+        app.serial,
+        app.name,
+        template.code AS template_code,
+        template.name AS template_name,
+        username AS applicant_username,
+        first_name AS applicant_first_name,
+        last_name AS applicant_last_name,
+        CONCAT(first_name, ' ', last_name) AS applicant,
+        org.name AS org_name,
+        stage_status.stage,
+        stage_status.status,
+        outcome,
+        status_history_time_created AS last_active_date,
+        -- 	template_questions_count(app),
+        -- 	assigned_questions_count(app, stage_status.stage_id, 1),
+        assigned_questions_count (app, stage_status.stage_id, 1) >= template_questions_count (app) AS is_fully_assigned_level_1,
+        review_available_for_self_assignment_count,
+        review_assigned_count,
+        review_assigned_not_started_count,
+        review_draft_count,
+        review_submitted_count,
+        review_change_request_count,
+        review_pending_count
+    FROM
+        application app
+    LEFT JOIN TEMPLATE ON app.template_id = template.id
+    LEFT JOIN "user" ON user_id = "user".id
+    LEFT JOIN application_stage_status_latest AS stage_status ON app.id = stage_status.application_id
+    LEFT JOIN organisation org ON app.org_id = org.id
+    LEFT JOIN review_list ($1) ON app.id = review_list.application_id
+$$
+LANGUAGE sql
+STABLE;
+
+COMMENT ON FUNCTION application_list (reviewerid int) IS E'@sortable';
+
