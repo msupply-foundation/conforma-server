9647e30ce59ee7b0568b2a56f41e4d778b932548
andreievg <andrei.evguenov@gmail.com>
2021-03-19 16:24:15 +1300

    remove quotes from status

--- database/buildSchema/22_application_response.sql
+++ database/buildSchema/22_application_response.sql
@@ -8,7 +8,7 @@ CREATE TABLE public.application_response (
     id serial PRIMARY KEY,
     template_element_id integer REFERENCES public.template_element (id),
     application_id integer REFERENCES public.application (id),
-    "status" public.application_response_status DEFAULT 'SUBMITTED',
+    status public.application_response_status DEFAULT 'SUBMITTED',
     value jsonb,
     is_valid boolean,
     time_updated timestamptz DEFAULT CURRENT_TIMESTAMP
