f6190dfb6c03b35a30ae2d70925b02755fbefc26
andreievg <andrei.evguenov@gmail.com>
2021-03-21 21:23:33 +1300

    add filters and category

--- database/buildSchema/07_template.sql
+++ database/buildSchema/07_template.sql
@@ -2,6 +2,12 @@
 
 CREATE TYPE public.template_status AS ENUM ('Draft', 'Available', 'Disabled');
 
+CREATE TABLE public.template_category (
+    id serial primary key,
+    title varchar,
+    icon varchar
+);
+
 CREATE TABLE public.template (
     id serial primary key,
     name varchar,
@@ -10,5 +16,23 @@ CREATE TABLE public.template (
     start_message jsonb,
     status public.template_status,
     submission_message jsonb DEFAULT '{"value": "Thank you! Your application has been submitted."}'::jsonb,
-    version_timestamp timestamptz
+    version_timestamp timestamptz,
+    template_category_id integer references public.template_category(id)
 );
+
+
+CREATE TABLE public.filter (
+    id serial primary key,
+    code varchar,
+    icon varchar,
+    title varchar,
+    query jsonb,
+    user_role varchar
+);
+
+CREATE TABLE public.template_filter_join (
+    id serial primary key,
+    template_id integer references public.template(id),
+    template_filter_id integer references public.filter(id)
+);
+
