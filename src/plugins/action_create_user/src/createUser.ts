2bdd5a77e62ec0e2cb7af272f902dec2977c83e7
andreievg <andrei.evguenov@gmail.com>
2021-03-23 14:34:21 +1300

    fix bug

--- src/plugins/action_create_user/src/createUser.ts
+++ src/plugins/action_create_user/src/createUser.ts
@@ -1,4 +1,4 @@
-module.exports['createUser'] = async function ({ aplicationData, ...user }: any, DBConnect: any) {
+module.exports['createUser'] = async function ({ applicationData, ...user }: any, DBConnect: any) {
   try {
     console.log(`\nAdding new user: ${user.username}`)
     const result = await DBConnect.createUser(user)
