c63c42d653fe1f18f4654c259f598638a439f360
andreievg <andrei.evguenov@gmail.com>
2021-03-29 21:55:41 +1300

    fix pluralisation

--- database/takeSnapshot.js
+++ database/takeSnapshot.js
@@ -101,13 +101,26 @@ const getInputTypes = (types, tableNames) => {
       !type.name.startsWith('Delete')
   )
 
-  return noCreateUpdateOrDelete.filter(({ name }) =>
-    tableNames.includes(name.replace('Input', 's').toLowerCase())
-  )
+  console.log(noCreateUpdateOrDelete, tableNames)
+  return noCreateUpdateOrDelete.filter(({ name }) => {
+    console.log('looking for' + toPlural(name.replace('Input', '')))
+
+    return tableNames.includes(toPlural(name.replace('Input', '')).toLowerCase())
+  })
+}
+
+const toPlural = (string) => {
+  if (string.endsWith('y')) return string.slice(0, string.length - 1) + 'ies'
+  return string + 's'
+}
+
+const toSingular = (string) => {
+  if (string.endsWith('ies')) return string.slice(0, string.length - 3) + 'y'
+  return string.slice(0, string.length - 1)
 }
 
 const fromInputTypeToQueryType = (type) => {
-  const queryName = type.name.replace('Input', 's')
+  const queryName = toPlural(type.name.replace('Input', ''))
   const queryNameCamelCase = queryName[0].toLowerCase() + queryName.slice(1)
   return queryNameCamelCase
 }
@@ -116,7 +129,12 @@ const getQueries = (inputTypes) => {
   const queries = inputTypes.map((type) => {
     const queryName = fromInputTypeToQueryType(type)
     const fields = type.inputFields
-      .filter((field) => field.type.kind === 'SCALAR' || field.type.kind === 'NON_NULL')
+      .filter(
+        (field) =>
+          field.type.kind === 'SCALAR' ||
+          field.type.kind === 'NON_NULL' ||
+          field.type.kind === 'ENUM'
+      )
       .map((field) => field.name)
 
     const query = `query { ${queryName} { nodes {${fields.join(',')}} } }`
@@ -152,7 +170,7 @@ const cleanUpSnapshotFolder = async (folder) => {
 }
 
 const getRecordNameFromQueryName = (queryName) => {
-  return queryName.slice(0, queryName.length - 1)
+  return toSingular(queryName)
 }
 
 const getMutationNameFromQueryName = (queryName) => {
@@ -227,72 +245,5 @@ const saveMutationToFile = ({ mutation, query, record }) => {
 
   fs.writeFileSync(fileName, mutation)
 }
-
-//     queries.forEach(async (query) => {
-//       const data = await getData(query.gql, query.queryNameCamelCase)
-
-//       const typeName = query.queryNameCamelCase.slice(0, query.queryNameCamelCase.length - 1)
-
-//       const mutation = `create${typeName[0].toUpperCase() + typeName.slice(1)}`
-
-//       data.forEach((record) => {
-//         const fieldsAndValues = query.fields.map((field) => {
-//           let value = record[field]
-//           if (typeof record[field] === 'string') value = `${JSON.stringify(value)}`
-//           if (typeof record[field] === 'object' && record[field] !== null)
-//             value = noQuoteKeyStringify(value)
-
-//           return `${field}:` + value
-//         })
-
-//         const gqlMutation = `mutation { ${mutation}(input: {${typeName}: {${fieldsAndValues.join(
-//           ','
-//         )}}}) }`
-
-//         const dir = rootdir + '/' + typeName
-
-//         if (!fs.existsSync(dir)) {
-//           fs.mkdirSync(dir)
-//         }
-
-//         const generateFilname = graphQLdefinition.find(definition => definition.table === typeName)?.generateFileName || defaultGenerateFileName
-//         fs.writeFileSync(dir + '/' + generateFilname(record) + '.graphql', gqlMutation)
-//       })
-//     })
-//   })
-
-// const noQuoteKeyStringify = (json) => {
-//   const isArray = Array.isArray(json)
-
-//   let result = isArray ? '[' : '{'
-//   Object.entries(json).forEach(([key, value]) => {
-//     let resultValue = value
-//     if (typeof value === 'string') resultValue = `${JSON.stringify(value)}`
-//     if (Array.isArray(value)) resultValue = noQuoteKeyStringify(value)
-//     else if (typeof value === 'object' && value !== null) resultValue = noQuoteKeyStringify(value)
-
-//     if (isArray) result = result + resultValue + ','
-//     else result = result + key + ':' + resultValue + ','
-//   })
-
-//   if (result.length > 1) result = result.slice(0, result.length - 1)
-
-//   return result + (isArray ? ']' : '}')
-// }
-
-// const getData = async (query, queryNameCamelCase) => {
-//   const result = await fetch(graphQLendpoint, {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//       Accept: 'application/json',
-//     },
-//     body: JSON.stringify({
-//       query,
-//     }),
-//   })
-//   const json = await result.json()
-//   return json.data[queryNameCamelCase].nodes
-// }
 
 takeSnapshot()
