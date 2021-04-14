dec97d5233e26341484259e986a4150980e37cb6
andreievg <andrei.evguenov@gmail.com>
2021-04-14 15:44:47 +1200

    remove unused field from actions_plugin

--- database/buildSchema/23_action_plugin.sql
+++ database/buildSchema/23_action_plugin.sql
@@ -1,11 +1,9 @@
 -- action plugins
-
 CREATE TABLE public.action_plugin (
-    code varchar primary key,
+    code varchar PRIMARY KEY,
     name varchar,
     description varchar,
     path varchar,
-    function_name varchar,
     required_parameters varchar[],
     output_properties varchar[]
 );
