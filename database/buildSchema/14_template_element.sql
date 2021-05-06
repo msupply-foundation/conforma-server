7e08595fe213e43ba217b14b6039dbc7b2be3b70
andreievg <andrei.evguenov@gmail.com>
2021-05-06 15:05:02 +1200

    re-formatting using pgFormatter

--- database/buildSchema/14_template_element.sql
+++ database/buildSchema/14_template_element.sql
@@ -1,29 +1,40 @@
 -- template element (questions or information elements)
-
-CREATE TYPE public.template_element_category AS ENUM ('Question', 'Information');
+CREATE TYPE public.template_element_category AS ENUM (
+    'Question',
+    'Information'
+);
 
 -- FUNCTION to return template_code for current element/section
-CREATE or replace FUNCTION public.get_template_code(section_id int)
-RETURNS VARCHAR AS $$
-	SELECT template.code FROM template JOIN template_section
-	ON template_id = template.id
-	WHERE template_section.id = $1;
-$$ LANGUAGE SQL IMMUTABLE;
+CREATE OR REPLACE FUNCTION public.get_template_code (section_id int)
+    RETURNS varchar
+    AS $$
+    SELECT
+        template.code
+    FROM
+        TEMPLATE
+        JOIN template_section ON template_id = template.id
+    WHERE
+        template_section.id = $1;
+
+$$
+LANGUAGE SQL
+IMMUTABLE;
 
 CREATE TABLE public.template_element (
-    id serial primary key,
-    section_id integer references public.template_section(id),
+    id serial PRIMARY KEY,
+    section_id integer REFERENCES public.template_section (id),
     code varchar NOT NULL,
     index integer,
     title varchar,
     category public.template_element_category,
     element_type_plugin_code varchar,
-    visibility_condition jsonb DEFAULT '{"value":true}'::jsonb,
-    is_required jsonb DEFAULT '{"value":true}'::jsonb,
-    is_editable jsonb DEFAULT '{"value":true}'::jsonb,
-    validation jsonb DEFAULT '{"value":true}'::jsonb,
+    visibility_condition jsonb DEFAULT '{"value":true}' ::jsonb,
+    is_required jsonb DEFAULT '{"value":true}' ::jsonb,
+    is_editable jsonb DEFAULT '{"value":true}' ::jsonb,
+    validation jsonb DEFAULT '{"value":true}' ::jsonb,
     validation_message varchar,
     parameters jsonb,
-    template_code varchar GENERATED ALWAYS AS (public.get_template_code(section_id)) STORED,
+    template_code varchar GENERATED ALWAYS AS (public.get_template_code (section_id)) STORED,
     UNIQUE (template_code, code)
 );
+
--- database/buildSchema/18_application_stage_history.sql
+++ database/buildSchema/18_application_stage_history.sql
@@ -1,28 +1,32 @@
 -- application stage history
-
 CREATE TABLE public.application_stage_history (
-    id serial primary key,
-    application_id integer references public.application(id),
-    stage_id integer references public.template_stage(id),
-    time_created timestamptz default current_timestamp,
-    is_current bool DEFAULT true
+    id serial PRIMARY KEY,
+    application_id integer REFERENCES public.application (id),
+    stage_id integer REFERENCES public.template_stage (id),
+    time_created timestamptz DEFAULT CURRENT_TIMESTAMP,
+    is_current bool DEFAULT TRUE
 );
 
-
 -- FUNCTION to set `is_current` to false on all other stage_histories of current application
-CREATE OR REPLACE FUNCTION public.stage_is_current_update()
-RETURNS trigger as $application_stage_history_event$
+CREATE OR REPLACE FUNCTION public.stage_is_current_update ()
+    RETURNS TRIGGER
+    AS $application_stage_history_event$
 BEGIN
-	UPDATE public.application_stage_history SET is_current = false
-	WHERE application_id = NEW.application_id AND id<>NEW.id;
-RETURN NULL;
+    UPDATE
+        public.application_stage_history
+    SET
+        is_current = FALSE
+    WHERE
+        application_id = NEW.application_id
+        AND id <> NEW.id;
+    RETURN NULL;
 END;
 $application_stage_history_event$
 LANGUAGE plpgsql;
 
-
 --TRIGGER to run above function when is_current is updated
-CREATE TRIGGER application_stage_history_trigger AFTER INSERT OR UPDATE OF is_current ON public.application_stage_history
-FOR EACH ROW
-WHEN (NEW.is_current = true)
-EXECUTE FUNCTION public.stage_is_current_update()
+CREATE TRIGGER application_stage_history_trigger
+    AFTER INSERT OR UPDATE OF is_current ON public.application_stage_history
+    FOR EACH ROW
+    WHEN (NEW.is_current = TRUE)
+    EXECUTE FUNCTION public.stage_is_current_update ()
