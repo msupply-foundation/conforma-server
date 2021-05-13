35fbb5f0078030dd8d1bf7a73d83f571842e00e1
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2021-05-13 17:43:24 +1200

    sessionId NOT unique

--- database/buildSchema/16_application.sql
+++ database/buildSchema/16_application.sql
@@ -10,7 +10,7 @@ CREATE TABLE public.application (
     template_id integer REFERENCES public.template (id),
     user_id integer REFERENCES public.user (id),
     org_id integer REFERENCES public.organisation (id),
-    session_id varchar UNIQUE,
+    session_id varchar,
     serial varchar UNIQUE,
     name varchar,
     outcome public.application_outcome,
