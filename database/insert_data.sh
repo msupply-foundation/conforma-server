ed46a48f49f7f50af34b05b7d2696624bb8e2590
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2021-05-13 15:57:31 +1200

    Tidy row-level-policy scripts

--- database/insert_data.sh
+++ database/insert_data.sh
@@ -9,8 +9,8 @@ PID=$!
 wait $PID
 
 # Turn on Row-level Security
-# echo -e "\nEnabling Row-level Security..."
-# psql -U postgres -q -b -d tmf_app_manager -f "./database/turn_on_row_level_security.sql"
+echo -e "\nEnabling Row-level Security..."
+psql -U postgres -q -b -d tmf_app_manager -f "./database/turn_on_row_level_security.sql"
 # >&/dev/null #suppress output for this command
 
 node ./database/updateRowPolicies.js --from_insert_data.sh &
