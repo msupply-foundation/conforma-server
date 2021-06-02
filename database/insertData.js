dd11e199213ce45aabedc079ea8458961dd36aa6
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2021-06-02 15:19:19 +1200

    Update script and re-organise insertData files

--- database/insertData.js
+++ database/insertData.js
@@ -1,12 +1,35 @@
 const fs = require('fs')
+const path = require('path')
 const fetch = require('node-fetch')
 const config = require('../src/config.json')
 
+const dataFolder = './database/insertData'
+const sharedDataFiles = ['core_mutations.js', 'dev_actions.js']
+
 if (process.argv[2] === '--from_insert_data.sh') {
   const filesToProcess = fs
-    .readdirSync('./database/insertData')
+    .readdirSync(dataFolder, { withFileTypes: true })
+    .filter((dirent) => dirent.isFile())
+    .map((dirent) => dirent.name)
     .filter((file) => !file.match(/^\./)) // Ignore hidden files
     .filter((file) => !['core_mutations.js', 'dev_actions.js'].includes(file))
+
+  // Add locale-specific files
+  const localeFolder = process.argv[3]
+  if (localeFolder) {
+    console.log(`Locale: ${localeFolder}`)
+    const subfolderFilesToProcess = fs
+      .readdirSync(path.join(dataFolder, localeFolder))
+      .filter((file) => !file.match(/^\./)) // Ignore hidden files
+      .filter((file) => !sharedDataFiles.includes(file))
+    filesToProcess.push(
+      ...subfolderFilesToProcess.map((filename) => path.join(localeFolder, filename))
+    )
+    // Copy current version of core mutations/actions to subfolder
+    sharedDataFiles.forEach((file) =>
+      fs.copyFileSync(path.join(dataFolder, file), path.join(dataFolder, localeFolder, file))
+    )
+  }
 
   processQueries(filesToProcess)
 }
--- database/insertData/dev/04_users.js
+++ database/insertData/dev/04_users.js
@@ -4,32 +4,6 @@ Insert USERS (and some basic permission)
   -- Hashes generated with https://www.browserling.com/tools/bcrypt 
 */
 exports.queries = [
-  // Non Registered User with Permissions for UserRegistration only
-  // Password is blank
-  // Add nonRegisterd first, so it always gets ID 1
-  `mutation {
-    createUser(
-      input: {
-        user: {
-          email: ""
-          passwordHash: "$2a$10$UIfa3GTUbOS92Ygy/UpqheTngGo3O54Q5UOnJ5CBlra9LYCcr4IGq"
-          username: "nonRegistered"
-          permissionJoinsUsingId: {
-            create: {
-              # Apply UserRego
-              permissionNameToPermissionNameId: {
-                connectByName: { name: "applyUserRegistration" }
-              }
-            }
-          }
-        }
-      }
-    ) {
-      user {
-        username
-      }
-    }
-  }`,
   `mutation {
     createUser(
       input: {
