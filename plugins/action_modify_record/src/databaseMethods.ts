515543c54ba466d660ba05de549e1c0f136a6e99
andreievg <andrei.evguenov@gmail.com>
2021-06-03 00:47:25 +1200

    modifyRecords create tables and fields and joins application

--- plugins/action_modify_record/src/databaseMethods.ts
+++ plugins/action_modify_record/src/databaseMethods.ts
@@ -1,35 +1,32 @@
+import { DBConnectType } from '../../../src/components/databaseConnect'
+
 const databaseMethods = (DBConnect: any) => ({
-  doesRecordExist: async (tableName: string, matchField: string, value: any) => {
+  getRecordId: async (tableName: string, matchField: string, value: any) => {
     const text = `
-      SELECT COUNT(*) FROM "${tableName}"
+      SELECT id FROM "${tableName}"
       WHERE "${matchField}" = $1
     `
     try {
       const result = await DBConnect.query({ text, values: [value] })
-      return result.rows[0].count > 0
+      return result?.rows[0]?.id || 0
     } catch (err) {
       console.log(err.message)
       throw err
     }
   },
-  updateRecord: async (
-    tableName: string,
-    matchField: string,
-    matchValue: any,
-    record: { [key: string]: any }
-  ) => {
+  updateRecord: async (tableName: string, id: string, record: { [key: string]: any }) => {
     const placeholders = DBConnect.getValuesPlaceholders(record)
     const matchValuePlaceholder = `$${placeholders.length + 1}`
     const text = `
       UPDATE "${tableName}" SET (${Object.keys(record)})
       = (${DBConnect.getValuesPlaceholders(record)})
-      WHERE "${matchField}" = ${matchValuePlaceholder}
+      WHERE id = ${matchValuePlaceholder}
       RETURNING *
       `
     try {
       const result = await DBConnect.query({
         text,
-        values: [...Object.values(record), matchValue],
+        values: [...Object.values(record), id],
       })
       return { success: true, [tableName]: result.rows[0] }
     } catch (err) {
@@ -37,20 +34,88 @@ const databaseMethods = (DBConnect: any) => ({
       throw err
     }
   },
-  createRecord: async (tableName: string, record: { [key: string]: any }) => {
-    const text = `
-      INSERT INTO "${tableName}" (${Object.keys(record)}) 
-      VALUES (${DBConnect.getValuesPlaceholders(record)})
-      RETURNING *
-      `
+  createRecord: async (tableName: string, record: { [key: string]: any }) =>
+    await createRecord(tableName, record, DBConnect),
+  createTable: async (tableName: string) => {
+    const text = `CREATE TABLE "${tableName}" ( id serial PRIMARY KEY)`
+    console.log('creating table with statement: ', text)
     try {
-      const result = await DBConnect.query({ text, values: Object.values(record) })
-      return { success: true, [tableName]: result.rows[0] }
+      await DBConnect.query({
+        text,
+        value: [tableName],
+      })
     } catch (err) {
       console.log(err.message)
-      throw err
+      throw new Error(`Filed to create table: ${tableName}`)
+    }
+  },
+  createJoinTableAndRecord: async (tableName: string, applicationId: number, recordId: number) => {
+    // Create join table if one doesn't exist
+    const joinTableName = `application_${tableName}_join`
+    const text = `CREATE TABLE IF NOT EXISTS ${joinTableName}( 
+                  id serial PRIMARY KEY, 
+                  application_id integer references application(id),
+                  ${tableName}_id integer references "${tableName}"(id));`
+
+    console.log('creating join table with statement: ', text)
+    try {
+      await DBConnect.query({ text })
+    } catch (err) {
+      console.log(err.message)
+      throw new Error(`Filed to create join table: ${joinTableName}`)
+    }
+    // Create entry in the joiin table
+    const joinTableRecord = { application_id: applicationId, [`${tableName}_id`]: recordId }
+    await createRecord(joinTableName, joinTableRecord, DBConnect)
+  },
+  createFields: async (
+    tableName: string,
+    fieldsToCreate: { fieldName: string; fieldType: string }[]
+  ) => {
+    const getType = (fieldType: string) => (fieldType === 'object' ? 'jsonb' : 'varchar')
+    const newColumns = fieldsToCreate.map(
+      ({ fieldName, fieldType }) => `ADD COLUMN "${fieldName}" ${getType(fieldType)}`
+    )
+    const text = `ALTER TABLE "${tableName}" ${newColumns.join(',')};`
+    console.log('creating new columns with statement: ', text)
+    try {
+      await DBConnect.query({
+        text,
+        value: [tableName],
+      })
+    } catch (err) {
+      console.log(err.message)
+      throw new Error(
+        `Filed to create fields in table table: ${tableName} ${JSON.stringify(
+          fieldsToCreate,
+          null,
+          ' '
+        )}`
+      )
     }
   },
 })
+
+const createRecord = async (
+  tableName: string,
+  record: { [key: string]: any },
+  DBConnect: DBConnectType
+) => {
+  const text = `
+    INSERT INTO "${tableName}" (${Object.keys(record)}) 
+    VALUES (${DBConnect.getValuesPlaceholders(record)})
+    RETURNING *
+    `
+  try {
+    const result = await DBConnect.query({ text, values: Object.values(record) })
+    const firstRow = result.rows[0]
+    return { success: true, [tableName]: firstRow, recordId: firstRow.id }
+  } catch (err) {
+    console.log(err.message)
+    throw err
+  }
+}
+
+export type DatabaseMethodsType = ReturnType<typeof databaseMethods>
 
 export default databaseMethods
