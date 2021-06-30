2cdd377b5bf20d534c9d127297a25afb48792a64
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2021-06-30 17:29:39 +1200

    WIP

---
+++ src/components/files/documentGenerate.ts
@@ -0,0 +1,9 @@
+import path from 'path'
+import fs from 'fs'
+import util from 'util'
+import { getAppEntryPointDir } from '../utilityFunctions'
+import { nanoid } from 'nanoid'
+import config from '../../config'
+import carbone from 'carbone'
+
+export const generatePDF = (input: any) => {}
