423f04504b068c6df7f4e834e37db7481ab606a8
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2021-07-05 09:33:21 +1200

    Update dev_actions.js

--- database/insertData/_helpers/dev_actions.js
+++ database/insertData/_helpers/dev_actions.js
@@ -15,6 +15,35 @@ exports.devActions = `
     {
       actionCode: "sendNotification"
       trigger: DEV_TEST
+      sequence: 3
+      parameterQueries: {
+        fromName: "Application Manager"
+        fromEmail: "no-reply@sussol.net"
+        email: {
+          operator: "objectProperties"
+          children: ["applicationData.responses.Q4Email.text", ""]
+        }
+        subject: "User Registration - verify account"
+        message: {
+          operator: "stringSubstitution"
+          children: [
+            "Hi, %1,\\n\\nplease confirm your user account registration by clicking (or copy-pasting) the following link:\\n\\n%2%3"
+            {
+              operator: "objectProperties"
+              children: ["applicationData.responses.Q1FirstName.text", ""]
+            }
+            "http://localhost:3000/verify?uid=" #TO-DO: add website URL to back-end config
+            {
+              operator: "objectProperties"
+              children: ["outputCumulative.verification.unique_id"]
+            }
+          ]
+        }
+      }
+    }
+    {
+      actionCode: "sendNotification"
+      trigger: DEV_TEST
       sequence: 2
       parameterQueries: {
         subject: "Report attached"
