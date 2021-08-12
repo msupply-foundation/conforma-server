32dcce074b644169e6a5b4aaff16916415bc38ef
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2021-08-13 11:58:51 +1200

    Update dockerise for snapshot changes

--- utils/build_all.sh
+++ utils/build_all.sh
@@ -4,13 +4,13 @@ echo 'removing build folder'
 rm -rf ./build
 echo 'building app with tsc'
 tsc
-echo 'copying images' 
+echo 'copying images'
 cp -R images ./build/
 echo 'copying database related files'
 cp -R database/buildSchema ./build/database/
 cp -R database/insertData ./build/database/
 cp -R database/snapshotOptions ./build/database/
-cp -R database/snapshots ./build/database/
+cp -R database/_snapshots ./build/database/
 #cp -R database/snapshotScripts/*.html ./build/database/snapshotScripts
 cp -R database/*.sh build/database
 cp -R database/*.sql build/database
