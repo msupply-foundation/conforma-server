181aa055e3dda5ed756e2e0652a152bba6ea2291
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2021-09-01 12:33:21 +1200

    Manually create _snapshots folder during build

--- utils/build_all.sh
+++ utils/build_all.sh
@@ -4,13 +4,15 @@ echo 'removing build folder'
 rm -rf ./build
 echo 'building app with tsc'
 tsc
-echo 'copying images' 
+echo 'copying images'
 cp -R images ./build/
 echo 'copying database related files'
 cp -R database/buildSchema ./build/database/
 cp -R database/insertData ./build/database/
 cp -R database/snapshotOptions ./build/database/
 cp -R database/basic_snapshot ./build/database/
+# _snapshots needs to be created manually because not in repo (.gitignore)
+mkdir ./build/database/_snapshots
 echo 'copying import/export scripts for snapshots'
 cp -R database/*.sh build/database
 cp -R database/*.sql build/database
