7d3438fad7ce915205ee47f29785e8ebc147611a
nmadruga <katinachovansky@gmail.com>
2021-09-07 09:23:32 +1200

    Change target branches to test locally

--- docker/dockerise.sh
+++ docker/dockerise.sh
@@ -3,9 +3,9 @@
 # This command requires githubtoken.txt, in the repo root (it's git ingored btw).
 # githubtoken.txt should contain github token: https://github.com/settings/tokens -> generate new token -> [x] read:packages
 
-SERVER_BRANCH='B-1.0.2'
-WEB_APP_BRANCH='B-1.0.2'
-IMAGE_NAME='mflow-demo-2021-09-01'
+SERVER_BRANCH='main'
+WEB_APP_BRANCH='main'
+IMAGE_NAME='mflow-demo'
 INITIAL_DATA_LOCALE=''
 
 NODE_VERSION='14'
