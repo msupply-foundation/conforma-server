1ecc5eb637d5a9f5dd46653e82215da16656203b
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2021-09-10 11:10:04 +1200

    Update 44_outcome_display.sql

--- database/buildSchema/44_outcome_display.sql
+++ database/buildSchema/44_outcome_display.sql
@@ -1,23 +1,31 @@
+CREATE TYPE public.outcome_display_type AS ENUM (
+    'TABLE',
+    'DETAIL'
+);
+
 -- Outcome display
 CREATE TABLE outcome_display (
     id serial PRIMARY KEY,
     table_name varchar,
-    -- For graphQL query
-    plural_table_name varchar,
     title varchar,
     code varchar UNIQUE,
-    -- Title for detail view
-    detail_column_name varchar
+    table_view_include_columns varchar[] DEFAULT NULL,
+    table_view_exclude_columns varchar[] DEFAULT NULL,
+    detail_view_include_columns varchar[] DEFAULT NULL,
+    detail_view_exclude_columns varchar[] DEFAULT NULL,
+    -- Header for detail view
+    detail_view_header_column varchar,
+    display_type public.outcome_display_type DEFAULT NULL,
 );
 
--- Table display (when outcome is pressed)
-CREATE TABLE outcome_display_table (
+-- For columns that require more detail format or evaluation definitions
+CREATE TABLE outcome_display_column_definition (
     id serial PRIMARY KEY,
-    column_name varchar,
-    -- Text or full value from form element plugin
-    is_text_column boolean,
+    outcome_display_id integer REFERENCES outcome_display (id) ON DELETE CASCADE,
     title varchar,
-    outcome_display_id integer REFERENCES outcome_display (id) ON DELETE CASCADE
+    element_type_plugin_code varchar,
+    additional_formatting jsonb,
+    value jsonb NOT NULL
 );
 
 -- Details display when outcome is pressed in the table
