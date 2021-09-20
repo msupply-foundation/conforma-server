3c5f53d2a265c8c40b5f70e998d506403db9a745
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2021-09-20 15:07:53 +1200

    Remove timezone

--- database/buildSchema/02_user.sql
+++ database/buildSchema/02_user.sql
@@ -5,7 +5,7 @@ CREATE TABLE public.user (
     last_name varchar,
     username varchar UNIQUE,
     email varchar,
-    date_of_birth timestamptz,
+    date_of_birth timestamp,
     password_hash varchar
 );
 
