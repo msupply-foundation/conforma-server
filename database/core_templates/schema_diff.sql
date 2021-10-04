1a48e7c69ee4a63c29e85c245ae9226c628096f5
nmadruga <katinachovansky@gmail.com>
2021-10-04 14:09:55 +1300

    Add changes to templates: FDA company linked to admin and fix on User registration email

--- database/core_templates/schema_diff.sql
+++ database/core_templates/schema_diff.sql
@@ -1,7 +1,23 @@
 /*************************************************/
 /*** SCRIPT AUTHOR: application-manager-server ***/
-/***    CREATED ON: 2021-10-03T22:40:37.573Z   ***/
+/***    CREATED ON: 2021-10-04T00:48:09.083Z   ***/
 /*************************************************/
+
+--- BEGIN CREATE TABLE "public"."organisation_application_join" ---
+
+CREATE TABLE IF NOT EXISTS "public"."organisation_application_join" (
+	"id" serial NOT NULL  ,
+	"application_id" int4 NOT NULL  ,
+	"organisation_id" int4 NOT NULL  ,
+	CONSTRAINT "organisation_application_join_pkey" PRIMARY KEY (id) ,
+	CONSTRAINT "organisation_application_join_application_id_fkey" FOREIGN KEY (application_id) REFERENCES application(id) ON DELETE CASCADE ,
+	CONSTRAINT "organisation_application_join_organisation_id_fkey" FOREIGN KEY (organisation_id) REFERENCES organisation(id) ON DELETE CASCADE 
+);
+
+ALTER TABLE IF EXISTS "public"."organisation_application_join" OWNER TO postgres;
+
+
+--- END CREATE TABLE "public"."organisation_application_join" ---
 
 --- BEGIN CREATE TABLE "public"."user_application_join" ---
 
@@ -18,6 +34,27 @@ ALTER TABLE IF EXISTS "public"."user_application_join" OWNER TO postgres;
 
 
 --- END CREATE TABLE "public"."user_application_join" ---
+
+--- BEGIN ALTER TABLE "public"."organisation" ---
+
+ALTER TABLE IF EXISTS "public"."organisation" ADD COLUMN IF NOT EXISTS "registration_documentation" jsonb NULL  ;
+
+--- END ALTER TABLE "public"."organisation" ---
+
+--- BEGIN CREATE SEQUENCE "public"."organisation_application_join_id_seq" ---
+
+
+CREATE SEQUENCE IF NOT EXISTS "public"."organisation_application_join_id_seq" 
+	INCREMENT BY 1 
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START WITH 1
+	CACHE 1
+	NO CYCLE;
+
+ALTER SEQUENCE "public"."organisation_application_join_id_seq" OWNER TO postgres;
+
+--- END CREATE SEQUENCE "public"."organisation_application_join_id_seq" ---
 
 --- BEGIN CREATE SEQUENCE "public"."user_application_join_id_seq" ---
 
