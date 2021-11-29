06349071f4c04d3ccf3082eae970f4287ad854c3
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2021-11-30 10:06:50 +1300

    Improve SQL queries, handle refresh ALL

--- plugins/action_refresh_review_assignments/src/databaseMethods.ts
+++ plugins/action_refresh_review_assignments/src/databaseMethods.ts
@@ -15,16 +15,39 @@ const databaseMethods = (DBConnect: any) => ({
     }
   },
   getApplicationsFromUserPermissions: async (userIds: number[]) => {
-    const text = `SELECT DISTINCT app.id
-      FROM public.permissions_all perm
-      JOIN application app ON perm."templateId" = app.template_id
+    const text = `SELECT DISTINCT app.application_id
+      FROM permissions_all perm
+      JOIN application_stage_status_latest app
+      ON perm."templateId" = app.template_id
       WHERE "userId" = ANY($1)
       AND "permissionType" = ANY('{REVIEW, ASSIGN}')
       AND app.outcome = 'PENDING'
+      AND app.status <> 'DRAFT'
     `
     try {
       const result = await DBConnect.query({ text, values: [userIds] })
-      return result.rows.map(({ id }: { id: number }) => id)
+      return result.rows.map(({ application_id }: { application_id: number }) => application_id)
+    } catch (err) {
+      console.log(err.message)
+      throw err
+    }
+  },
+  getAllActiveApplications: async () => {
+    const text = `SELECT application_id FROM
+      application_stage_status_latest
+      WHERE template_id IN (
+        SELECT DISTINCT template.id
+        FROM template JOIN template_stage ts
+        ON template.id = template_id
+        JOIN template_stage_review_level rl
+        ON ts.id = rl.stage_id
+      )
+      AND outcome = 'PENDING'
+      AND status <> 'DRAFT'
+    `
+    try {
+      const result = await DBConnect.query({ text })
+      return result.rows.map(({ application_id }: { application_id: number }) => application_id)
     } catch (err) {
       console.log(err.message)
       throw err
--- plugins/action_refresh_review_assignments/src/refreshReviewAssignments.ts
+++ plugins/action_refresh_review_assignments/src/refreshReviewAssignments.ts
@@ -10,28 +10,37 @@ async function refreshReviewAssignments({
 }: ActionPluginInput) {
   const db = databaseMethods(DBConnect)
 
-  if (!parameters?.userId)
-    return {
-      status: ActionQueueStatus.Fail,
-      error_log: 'No userId(s) provided',
-    }
+  // If no userId parameter, we will update ALL active applications
+  const shouldRefreshAll = !parameters?.userId
 
   // Can handle input of either single userId or an array of userIds
   const userIds = Array.isArray(parameters?.userId) ? parameters.userId : [parameters.userId]
 
   try {
-    // Get active applications with existing review assignments for users
-    const applicationIds = await db.getApplicationsWithExistingReviewAssignments(userIds)
+    const getApplicationsToUpdate = async (userIds: number[]) => {
+      // Get active applications with existing review assignments for users
+      const applicationIds = await db.getApplicationsWithExistingReviewAssignments(userIds)
 
-    // Get active applications based on users' permissions
-    applicationIds.push(...(await db.getApplicationsFromUserPermissions(userIds)))
+      // Get active applications based on users' permissions
+      applicationIds.push(...(await db.getApplicationsFromUserPermissions(userIds)))
 
-    const distinctApplicationIds = Array.from(new Set(applicationIds))
+      return Array.from(new Set(applicationIds)) // Makes list elements unique
+    }
+
+    const applicationIds = shouldRefreshAll
+      ? await db.getAllActiveApplications()
+      : await getApplicationsToUpdate(userIds)
+
+    console.log(
+      shouldRefreshAll
+        ? 'Refreshing review_assignments for ALL active applications...'
+        : 'Refreshing review_assignments for applications:' + applicationIds
+    )
 
     // Iterate over applications and call "generateReviewAssignments" action for
     // each one
     const resultsArray = []
-    for (const applicationId of distinctApplicationIds) {
+    for (const applicationId of applicationIds) {
       const output = await generateReviewAssignments({
         parameters: { applicationId },
         DBConnect,
@@ -43,7 +52,7 @@ async function refreshReviewAssignments({
       status: ActionQueueStatus.Success,
       error_log: '',
       output: {
-        updatedApplications: distinctApplicationIds,
+        updatedApplications: applicationIds,
         reviewAssignments: resultsArray,
       },
     }
