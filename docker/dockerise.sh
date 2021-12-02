7e240e6fadde6cc2de83947f59cad2b513da07f5
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2021-12-03 09:57:18 +1300

    Update dockerise.sh

--- docker/dockerise.sh
+++ docker/dockerise.sh
@@ -3,8 +3,8 @@
 # This command requires githubtoken.txt, in the repo root (it's git ingored btw).
 # githubtoken.txt should contain github token: https://github.com/settings/tokens -> generate new token -> [x] read:packages
 
-SERVER_BRANCH='B-1.0.10'
-WEB_APP_BRANCH='B-1.0.10'
+SERVER_BRANCH='B-1.0.11'
+WEB_APP_BRANCH='B-1.0.11'
 IMAGE_NAME='conforma-demo'
 INITIAL_DATA_LOCALE=''
 
