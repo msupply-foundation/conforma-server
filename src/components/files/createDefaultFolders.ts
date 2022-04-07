944dda9493fcd27ff535aa123d7ff309e17bd825
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2022-04-07 23:29:59 +1200

    Copy core_templates with createDefaultFolders

--- src/components/files/createDefaultFolders.ts
+++ src/components/files/createDefaultFolders.ts
@@ -1,5 +1,6 @@
-// The first time the server loads, we want to populate prefs, files, snapshots
-// and localisation with default content from core_templates
+// Creates (and populates if appropriate) the folders that are not saved as part
+// of git repo. We run this for safety when starting the server, or before
+// saving or loading a snapshot.
 
 import {
   PREFERENCES_FOLDER,
@@ -10,6 +11,8 @@ import {
   BASE_SNAPSHOT_FOLDER,
   GENERIC_THUMBNAILS_FOLDER,
   GENERIC_THUMBNAILS_SOURCE_FOLDER,
+  DATABASE_FOLDER,
+  BASE_SNAPSHOT_NAME,
 } from '../../constants'
 import fs from 'fs'
 import path from 'path'
@@ -18,14 +21,18 @@ import { makeFolder } from '../utilityFunctions'
 
 export function createDefaultDataFolders() {
   try {
+    console.log('Creating SNAPSHOTS folder')
     makeFolder(SNAPSHOT_FOLDER)
+    // Copy core_templates to snapshots folder
+    execSync(`cp -r '${DATABASE_FOLDER}/${BASE_SNAPSHOT_NAME}' '${SNAPSHOT_FOLDER}'`)
   } catch {
     console.log('\nProblem creating SNAPSHOTS folder\n')
   }
+
   try {
+    console.log('Creating FILES folder')
     makeFolder(FILES_FOLDER)
-    // Restore generic thumbnails, they sometimes get wiped out during snapshot
-    // loading
+    // Restore generic thumbnails, they get wiped out during snapshot loading
     makeFolder(path.join(GENERIC_THUMBNAILS_FOLDER))
     execSync(`cp -r '${GENERIC_THUMBNAILS_SOURCE_FOLDER}/.' '${GENERIC_THUMBNAILS_FOLDER}'`)
   } catch {
@@ -35,6 +42,7 @@ export function createDefaultDataFolders() {
   // If localisation folder is missing, we assume it's a fresh install and
   // fetch them (and preferences) from core templates
   try {
+    console.log('Restoring LOCALISATIONS and PREFERENCES')
     if (
       !fs.existsSync(LOCALISATION_FOLDER) ||
       !fs.existsSync(path.join(LOCALISATION_FOLDER, 'languages.json'))
