cfa3123108a07b3aa3fb34fd72bf17b8701f6bbb
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2022-04-10 08:56:55 +1200

    Abstract read/write options to file

--- src/components/localisation/routes.ts
+++ src/components/localisation/routes.ts
@@ -28,9 +28,7 @@ export const routeGetLanguageFile = async (request: any, reply: any) => {
 
 export const routeEnableLanguage = async (request: any, reply: any) => {
   const { code, enabled } = combineRequestParams(request)
-  const languageOptions: LanguageOption[] = JSON.parse(
-    readFileSync(path.join(getAppEntryPointDir(), '../localisation/languages.json'), 'utf8')
-  )
+  const languageOptions: LanguageOption[] = readLanguageOptions()
   const index = languageOptions.findIndex((language) => language.code === code)
   if (index === -1) return reply.send({ success: false, message: 'Code not recognised' })
 
@@ -46,10 +44,7 @@ export const routeEnableLanguage = async (request: any, reply: any) => {
 
   // Write file back
   try {
-    await writeFilePromise(
-      path.join(getAppEntryPointDir(), '../localisation/languages.json'),
-      JSON.stringify(languageOptions, null, 2)
-    )
+    await writeLanguageOptions(languageOptions)
     return reply.send({ success: true, data: languageOptions })
   } catch (err) {
     return reply.send({
@@ -66,9 +61,8 @@ export const routeInstallLanguage = async (request: any, reply: any) => {
   if (!checkStrings(strings))
     return reply.send({ success: false, message: 'Incorrect strings format' })
 
-  const languageOptions = JSON.parse(
-    readFileSync(path.join(getAppEntryPointDir(), '../localisation/languages.json'), 'utf8')
-  )
+  const languageOptions = readLanguageOptions()
+
   const index = languageOptions.findIndex((lang: LanguageOption) => language.code === lang.code)
   let message: string
   if (index === -1) {
@@ -81,10 +75,7 @@ export const routeInstallLanguage = async (request: any, reply: any) => {
 
   // Write files
   try {
-    await writeFilePromise(
-      path.join(getAppEntryPointDir(), '../localisation/languages.json'),
-      JSON.stringify(languageOptions, null, 2)
-    )
+    await writeLanguageOptions(languageOptions)
     makeFolder(path.join(getAppEntryPointDir(), `../localisation/${language.code}/`))
 
     await writeFilePromise(
@@ -99,9 +90,7 @@ export const routeInstallLanguage = async (request: any, reply: any) => {
 
 export const routeRemoveLanguage = async (request: any, reply: any) => {
   const { code } = combineRequestParams(request)
-  const languageOptions = JSON.parse(
-    readFileSync(path.join(getAppEntryPointDir(), '../localisation/languages.json'), 'utf8')
-  )
+  const languageOptions = readLanguageOptions()
 
   const newLanguageOptions = languageOptions.filter(
     (language: LanguageOption) => language.code !== code
@@ -113,10 +102,7 @@ export const routeRemoveLanguage = async (request: any, reply: any) => {
 
   // Write options file and delete language file
   try {
-    await writeFilePromise(
-      path.join(getAppEntryPointDir(), '../localisation/languages.json'),
-      JSON.stringify(newLanguageOptions, null, 2)
-    )
+    await writeLanguageOptions(newLanguageOptions)
     await rmdirSync(path.join(getAppEntryPointDir(), `../localisation/${code}`), {
       recursive: true,
     })
@@ -128,6 +114,16 @@ export const routeRemoveLanguage = async (request: any, reply: any) => {
     })
   }
 }
+const readLanguageOptions = (): LanguageOption[] =>
+  JSON.parse(
+    readFileSync(path.join(getAppEntryPointDir(), '../localisation/languages.json'), 'utf8')
+  )
+
+const writeLanguageOptions = async (languageOptions: LanguageOption[]) =>
+  await writeFilePromise(
+    path.join(getAppEntryPointDir(), '../localisation/languages.json'),
+    JSON.stringify(languageOptions, null, 2)
+  )
 
 const checkLanguageFormat = (language: LanguageOption) => {
   return (
