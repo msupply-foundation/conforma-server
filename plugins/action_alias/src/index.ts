8a4f26eb0fe975abc3e29d59a190d70ec1d5c737
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2022-06-30 19:58:32 +1200

    Create Action aliasing functionality

---
+++ plugins/action_alias/src/index.ts
@@ -0,0 +1,4 @@
+// Dummy file so the system thinks this plugin exists and template_elements can
+// be created for it. But all the alias logic is handled internally by the
+// triggersAndActions module
+export {}
---
+++ src/components/actions/helpers.ts
@@ -0,0 +1,30 @@
+import { ActionInTemplate } from '../../types'
+import DBConnect from '../databaseConnect'
+
+export const swapOutAliasedActions = async (templateId: number, action: ActionInTemplate) => {
+  // Fetch aliased action from database
+  const {
+    condition,
+    parameter_queries: { code, condition: altCondition, ...overrideParams },
+  } = action
+  if (!code) throw new Error('Missing code in aliased action')
+
+  const aliasedAction = await DBConnect.getSingleTemplateAction(templateId, code)
+
+  if (!aliasedAction) throw new Error('No Action matching alias')
+
+  // Override condition if specified
+  // It would make most sense for actual condition field to be the one we look
+  // at for overriding the aliased action's condition, but becauase this has
+  // default "true" and can never be null, then we'd always get "true" as the
+  // condition. So we only consider the condition field if it's something other
+  // than "true". And if we actually *want* it to be "true", then we can
+  // override it further by specifying a "condition" field in parameters
+  if (condition !== true) aliasedAction.condition = condition
+  if (altCondition !== undefined) aliasedAction.condition = altCondition
+
+  // Override parameters
+  aliasedAction.parameter_queries = { ...aliasedAction.parameter_queries, ...overrideParams }
+
+  return aliasedAction
+}
