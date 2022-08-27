17ca228d39ccfddbd309ddb13dfa53ba3b0f4e22
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2022-08-27 15:00:32 +1200

    Launch script, delete prefs from repo

---
+++ src/loadPrefs.ts
@@ -0,0 +1,19 @@
+/*
+Script to ensure preferences are present before server loads. Checks for /preferences/preferences.json and if not present copies it from core_templates.
+
+Called by nodemon before launching server.ts
+*/
+import fs from 'fs'
+import { execSync } from 'child_process'
+import { makeFolder } from './components/utilityFunctions'
+
+try {
+  if (!fs.existsSync('preferences') || !fs.existsSync('preferences/preferences.json')) {
+    console.log('Trying...')
+    makeFolder('preferences', 'Restoring default preferences...\n')
+    execSync('cp database/core_templates/preferences.json preferences')
+  }
+} catch {
+  console.log('\nProblem restoring default preferences\n')
+  process.exit(1)
+}
