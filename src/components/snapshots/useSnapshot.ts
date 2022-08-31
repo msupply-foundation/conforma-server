2f9c5922bf10e7d22ab8a4de93cac9a32b4cf0e3
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2022-08-31 15:31:52 +1200

    Clear out localisation and prefs folder

--- src/components/snapshots/useSnapshot.ts
+++ src/components/snapshots/useSnapshot.ts
@@ -27,6 +27,7 @@ import {
   PREFERENCES_FILE,
   SCHEMA_FILE_NAME,
   INFO_FILE_NAME,
+  PREFERENCES_FOLDER,
 } from '../../constants'
 
 const useSnapshot: SnapshotOperation = async ({
@@ -97,6 +98,7 @@ const useSnapshot: SnapshotOperation = async ({
     // Import localisations
     if (options?.includeLocalisation) {
       try {
+        execSync(`rm -rf ${LOCALISATION_FOLDER}/*`)
         execSync(`cp -r  '${snapshotFolder}/localisation/.' '${LOCALISATION_FOLDER}' `)
       } catch (e) {
         console.log("Couldn't import localisations")
@@ -106,6 +108,7 @@ const useSnapshot: SnapshotOperation = async ({
     // Import preferences
     if (options?.includePrefs) {
       try {
+        execSync(`rm -rf ${PREFERENCES_FOLDER}/*`)
         execSync(`cp '${snapshotFolder}/preferences.json' '${PREFERENCES_FILE}'`)
       } catch (e) {
         console.log("Couldn't import preferences")
