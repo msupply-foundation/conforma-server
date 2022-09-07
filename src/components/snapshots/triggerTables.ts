5a56dc8f2f7eafcc0c81173ff28ef48430d8c60e
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2022-09-08 10:11:47 +1200

    Update triggerTables.ts

--- src/components/snapshots/triggerTables.ts
+++ src/components/snapshots/triggerTables.ts
@@ -1,8 +1,17 @@
 // Keep this list up to date!
 
-// A list of tables whose triggers should be suppressed during snapshot data
-// insert, so they don't create extraneous records in the activity_log, or other
-// unwanted behaviours.
+/*
+A list of tables whose triggers should be suppressed during snapshot data
+insert, so they don't create extraneous records in the activity_log, or other
+unwanted behaviours.
+
+Get a list of all tables that have triggers with the following query:
+
+SELECT  event_object_table AS table_name ,trigger_name         
+FROM information_schema.triggers  
+GROUP BY table_name , trigger_name 
+ORDER BY table_name ,trigger_name;
+*/
 
 export const triggerTables = [
   'action_queue',
