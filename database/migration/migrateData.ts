7f788d4ed4fe52f75667465cb1292f38e17b1869
Nicole Madruga <katinachovansly@gmail.com>
2022-09-08 11:23:29 +1200

    Quick fix to not show action to Assign on list for Assigner

--- database/migration/migrateData.ts
+++ database/migration/migrateData.ts
@@ -1061,6 +1061,64 @@ const migrateData = async () => {
     $application_event$
     LANGUAGE plpgsql;
       `)
+
+    // Fix assigner_actions to only return active applications
+    console.log(' - Fix assigner_actions to only return active applications')
+
+    await DB.changeSchema(`
+    CREATE OR REPLACE FUNCTION assigner_list (stage_id int, assigner_id int)
+    RETURNS TABLE (
+        application_id int,
+        assigner_action public.assigner_action,
+        -- is_fully_assigned_level_1 boolean,
+        -- assigned_questions_level_1 bigint,
+        total_questions bigint,
+        total_assigned bigint,
+        total_assign_locked bigint
+    )
+    AS $$
+    SELECT
+        review_assignment.application_id AS application_id,
+        CASE WHEN COUNT(DISTINCT (review_assignment.id)) != 0
+            AND assigned_questions_count (application_id, $1, level_number) >= assignable_questions_count (application_id)
+            AND submitted_assigned_questions_count (application_id, $1, level_number) < assigned_questions_count (application_id, $1, level_number) THEN
+            'RE_ASSIGN'
+        WHEN COUNT(DISTINCT (review_assignment.id)) != 0
+            AND assigned_questions_count (application_id, $1, level_number) >= assignable_questions_count (application_id)
+            AND submitted_assigned_questions_count (application_id, $1, level_number) >= assigned_questions_count (application_id, $1, level_number) THEN
+            'ASSIGN_LOCKED'
+        WHEN COUNT(DISTINCT (review_assignment.id)) != 0
+            AND assigned_questions_count (application_id, $1, level_number) < assignable_questions_count (application_id) THEN
+            'ASSIGN'
+        ELSE
+            NULL
+        END::assigner_action,
+        -- assigned_questions_count(application_id, $1, 1) = assignable_questions_count(application_id) AS is_fully_assigned_level_1,
+        -- assigned_questions_count(application_id, $1, 1) AS assigned_questions_level_1,
+        assignable_questions_count (application_id) AS total_questions,
+        assigned_questions_count (application_id, $1, level_number) AS total_assigned,
+        submitted_assigned_questions_count (application_id, $1, level_number) AS total_assign_locked
+    FROM
+        review_assignment
+    LEFT JOIN review_assignment_assigner_join ON review_assignment.id = review_assignment_assigner_join.review_assignment_id
+WHERE
+    review_assignment.stage_id = $1
+    AND review_assignment_assigner_join.assigner_id = $2
+    AND (
+        SELECT
+            outcome
+        FROM
+            application
+        WHERE
+            id = review_assignment.application_id) = 'PENDING'
+GROUP BY
+    review_assignment.application_id,
+    review_assignment.level_number;
+
+$$
+LANGUAGE sql
+STABLE;
+    `)
   }
 
   // Other version migrations continue here...
