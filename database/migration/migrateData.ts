ca2a00ef7fcd7adc7c2547bd3563dea32cc027e9
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2022-09-07 16:54:08 +1200

    Update migrateData.ts

--- database/migration/migrateData.ts
+++ database/migration/migrateData.ts
@@ -1061,6 +1061,14 @@ const migrateData = async () => {
     $application_event$
     LANGUAGE plpgsql;
       `)
+
+    console.log(
+      ' - Change foreign key constraint on file table to allow changing of application serial'
+    )
+    await DB.changeSchema(`
+      ALTER TABLE file DROP CONSTRAINT IF EXISTS file_application_serial_fkey; 
+      ALTER TABLE file ADD CONSTRAINT file_application_serial_fkey FOREIGN KEY (application_serial) REFERENCES application (serial) ON UPDATE CASCADE;
+    `)
   }
 
   // Other version migrations continue here...
