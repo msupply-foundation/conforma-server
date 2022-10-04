d54bf49f60cb2a7909db90a9c3e83dd8b62867ed
Nicole Madruga <katinachovansly@gmail.com>
2022-10-04 15:43:24 +1300

    Moving work to improve functions and remove accounts not used in Application List

--- database/buildSchema/16_template_element.sql
+++ database/buildSchema/16_template_element.sql
@@ -7,6 +7,7 @@ CREATE TYPE public.template_element_category AS ENUM (
 CREATE TYPE public.is_reviewable_status AS ENUM (
     'ALWAYS',
     'NEVER',
+    'ONLY_IF_APPLICANT_ANSWER',
     'OPTIONAL_IF_NO_RESPONSE'
     -- TO-DO:
     -- 'OPTIONAL_IF_RESPONSE',
@@ -61,7 +62,7 @@ CREATE TABLE public.template_element (
     validation_message varchar,
     help_text varchar,
     parameters jsonb,
-    is_reviewable public.is_reviewable_status DEFAULT NULL,
+    is_reviewable public.is_reviewable_status DEFAULT 'ONLY_IF_APPLICANT_ANSWER' NOT NULL,
     -- review_required boolean NOT NULL DEFAULT TRUE,
     template_code varchar GENERATED ALWAYS AS (public.get_template_code (section_id)) STORED,
     template_version integer GENERATED ALWAYS AS (public.get_template_version (section_id)) STORED,
