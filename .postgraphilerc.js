b624dc8aee6622f2713cfe470fc010097b51d26f
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2022-10-17 12:58:11 +1300

    Update .postgraphilerc.js

--- .postgraphilerc.js
+++ .postgraphilerc.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 module.exports = {
   options: {
-    watchPg: true,
+    watch: true,
     appendPlugins: [
       '@graphile-contrib/pg-simplify-inflector',
       'postgraphile-plugin-nested-mutations',
