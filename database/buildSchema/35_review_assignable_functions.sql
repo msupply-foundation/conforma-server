a6686494497db82a6da06b8fa8421fb378a06609
Nicole Madruga <katinachovansly@gmail.com>
2022-10-17 15:28:06 +1300

    Add new function to check for all approved/agreed

--- database/buildSchema/35_review_assignable_functions.sql
+++ database/buildSchema/35_review_assignable_functions.sql
@@ -141,3 +141,19 @@ $$
 LANGUAGE sql
 STABLE;
 
+-- Checks if TOTAL of assigned questions are approved - used for completeness of assignable questions
+CREATE FUNCTION public.approved_assigned_questions_count (app_id int, stage_id int, level_number int)
+    RETURNS bigint
+    AS $$
+    SELECT
+        COUNT(*)
+    FROM
+        assigned_questions (app_id, stage_id, level_number) aq
+WHERE
+    aq.review_response_status = 'SUBMITTED'
+    AND (aq.decision = 'APPROVE'
+        OR aq.decision = 'AGREE')
+$$
+LANGUAGE sql
+STABLE;
+
--- database/migration/migrateData.ts
+++ database/migration/migrateData.ts
@@ -1448,6 +1448,24 @@ const migrateData = async () => {
           DEFAULT CURRENT_TIMESTAMP NOT NULL,
         ADD COLUMN IF NOT EXISTS email_server_log varchar; 
     `)
+
+    console.log(' - new function to heck if Review level is fully APROVED for assignable elements')
+    await DB.changeSchema(`
+    CREATE OR REPLACE FUNCTION public.approved_assigned_questions_count (app_id int, stage_id int, level_number int)
+      RETURNS bigint
+      AS $$
+        SELECT
+            COUNT(*)
+        FROM
+            assigned_questions (app_id, stage_id, level_number) aq
+    WHERE
+        aq.review_response_status = 'SUBMITTED'
+        AND (aq.decision = 'APPROVE'
+            OR aq.decision = 'AGREE')
+    $$
+    LANGUAGE sql
+    STABLE;
+    `)
   }
   // Other version migrations continue here...
 
