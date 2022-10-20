49143ce6d7b8a876a145b47303d0e95def3f96a3
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2022-10-20 17:06:04 +1300

    Update 02_user.sql
    Updates user build schema

--- database/buildSchema/02_user.sql
+++ database/buildSchema/02_user.sql
@@ -1,16 +1,17 @@
+-- adds citext constraint to username for case insensitivity
+CREATE EXTENSION IF NOT EXISTS citext;
+
 -- user table
 CREATE TABLE public.user (
     id serial PRIMARY KEY,
     first_name varchar,
     last_name varchar,
     full_name varchar GENERATED ALWAYS AS (first_name || ' ' || last_name) STORED,
-    username varchar UNIQUE,
+    username citext UNIQUE,
     email varchar,
     date_of_birth date,
     password_hash varchar
 );
 
--- adds citext constraint to username for case insensitivity
-CREATE EXTENSION IF NOT EXISTS citext;
 ALTER TABLE public.user ALTER COLUMN username TYPE citext;
 
