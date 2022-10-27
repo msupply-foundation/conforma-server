767f56f05190d3c9432ef1251013ce56c3eff813
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2022-10-27 19:25:42 +1300

    Update types

--- src/components/actions/dev_tools/eventTesting.test.ts
+++ src/components/actions/dev_tools/eventTesting.test.ts
@@ -1,17 +1,35 @@
 // Configure a snapshot to be used with this test suite.
 import { readFileSync } from 'fs'
 import { loadActionPlugins } from '../../pluginsConnect'
-import { testTrigger } from './routes'
+import { RequestProps, testTrigger } from './routes'
 import useSnapshot from '../../../components/snapshots/useSnapshot'
 import { TEST_SCRIPT_FOLDER, SNAPSHOT_FOLDER } from '../../../constants'
 
+interface TestData {
+  name: string
+  input: RequestProps
+  output: Partial<ReturnType<typeof testTrigger>>
+}
+
+interface TestScript {
+  snapshot: string
+  tests: TestData[]
+}
+
 const testFile = process.argv.pop()
 
+if (!testFile) {
+  console.log('No snapshot or test script provided')
+  process.exit(1)
+}
+
 // Read test json file
-let testData: any
+let testData: TestData[]
 let snapshotName: string
 try {
-  const testScript = JSON.parse(readFileSync(`${TEST_SCRIPT_FOLDER}/${testFile}.json`, 'utf-8'))
+  const testScript: TestScript = JSON.parse(
+    readFileSync(`${TEST_SCRIPT_FOLDER}/${testFile}.json`, 'utf-8')
+  )
   testData = testScript.tests
   snapshotName = testScript.snapshot
 } catch {
@@ -30,7 +48,7 @@ beforeAll((done) => {
   loadActionPlugins()
 })
 
-testData.forEach(({ name, input, output }: any) => {
+testData.forEach(({ name, input, output }) => {
   test(name, async () => {
     const result = await testTrigger(input)
     expect(result).toMatchObject(output)
