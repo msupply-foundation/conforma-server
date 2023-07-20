df100a2ee2d1e2592dcf4a3dfe589ac73461b9f4
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2023-07-20 17:22:50 +1200

    Consolidate Snapshot List methods

--- src/components/snapshots/routes/helpers.ts
+++ src/components/snapshots/routes/helpers.ts
@@ -15,30 +15,35 @@ import { DateTime } from 'luxon'
 
 export const timestampStringExpression = /_\d\d\d\d-\d\d-\d\d_\d\d-\d\d-\d\d$/
 
-export const getSnapshotList = async () => {
-  const dirents = await fs.readdir(SNAPSHOT_FOLDER, { encoding: 'utf-8', withFileTypes: true })
+export const getSnapshotList = async (archive?: boolean) => {
+  const sourceFolder = archive
+    ? path.join(SNAPSHOT_FOLDER, SNAPSHOT_ARCHIVES_FOLDER_NAME)
+    : SNAPSHOT_FOLDER
+
+  const dirents = await fs.readdir(sourceFolder, { encoding: 'utf-8', withFileTypes: true })
+
   const snapshots: (SnapshotInfo & { name: string; size: number })[] = []
 
   for (const dirent of dirents) {
     if (!dirent.isDirectory()) continue
+    if (!fsSync.existsSync(path.join(sourceFolder, dirent.name, `${INFO_FILE_NAME}.json`))) continue
     if (
+      !archive &&
       !(
-        fsSync.existsSync(path.join(SNAPSHOT_FOLDER, dirent.name, `${SNAPSHOT_FILE_NAME}.json`)) ||
-        fsSync.existsSync(path.join(SNAPSHOT_FOLDER, dirent.name, `database.dump`))
+        (await fse.pathExists(path.join(sourceFolder, dirent.name, `database.dump`))) ||
+        (await fse.pathExists(path.join(sourceFolder, dirent.name, `${SNAPSHOT_FILE_NAME}.json`)))
       )
     )
       continue
 
     let size: number | null = null
     try {
-      size = (await fs.stat(path.join(SNAPSHOT_FOLDER, `${dirent.name}.zip`))).size
+      size = (await fs.stat(path.join(sourceFolder, `${dirent.name}.zip`))).size
     } catch {
       size = null
     }
 
-    const info = await fse.readJson(
-      path.join(SNAPSHOT_FOLDER, dirent.name, `${INFO_FILE_NAME}.json`)
-    )
+    const info = await fse.readJson(path.join(sourceFolder, dirent.name, `${INFO_FILE_NAME}.json`))
 
     const name = dirent.name.replace(timestampStringExpression, '')
 
@@ -48,38 +53,6 @@ export const getSnapshotList = async () => {
   snapshots.sort(
     (a, b) => DateTime.fromISO(b.timestamp).toMillis() - DateTime.fromISO(a.timestamp).toMillis()
   )
-
-  return snapshots
-}
-
-export const getSnapshotArchiveList = async () => {
-  const archiveSnapshotFolder = path.join(SNAPSHOT_FOLDER, SNAPSHOT_ARCHIVES_FOLDER_NAME)
-  const dirents = await fs.readdir(archiveSnapshotFolder, {
-    encoding: 'utf-8',
-    withFileTypes: true,
-  })
-  const snapshots: (SnapshotInfo & { name: string })[] = []
-
-  for (const dirent of dirents) {
-    if (!dirent.isDirectory()) continue
-    if (!fsSync.existsSync(path.join(archiveSnapshotFolder, dirent.name, `${INFO_FILE_NAME}.json`)))
-      continue
-
-    const info = await fse.readJson(
-      path.join(archiveSnapshotFolder, dirent.name, `${INFO_FILE_NAME}.json`)
-    )
-
-    let size: number | null = null
-    try {
-      size = (await fs.stat(path.join(archiveSnapshotFolder, `${dirent.name}.zip`))).size
-    } catch {
-      size = null
-    }
-
-    const name = dirent.name.replace(timestampStringExpression, '')
-
-    snapshots.push({ name, filename: dirent.name, size, ...info })
-  }
 
   return snapshots
 }
--- src/components/snapshots/routes/routeListSnapshots.ts
+++ src/components/snapshots/routes/routeListSnapshots.ts
@@ -1,10 +1,10 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
-import { getCurrentArchiveList, getSnapshotArchiveList, getSnapshotList } from './helpers'
+import { getCurrentArchiveList, getSnapshotList } from './helpers'
 
 const routeListSnapshots = async (request: FastifyRequest, reply: FastifyReply) => {
   const currentArchives = await getCurrentArchiveList()
   const type = (request.query as { archive?: 'true' })?.archive ? 'archives' : 'snapshots'
-  const snapshots = type === 'snapshots' ? await getSnapshotList() : await getSnapshotArchiveList()
+  const snapshots = type === 'snapshots' ? await getSnapshotList() : await getSnapshotList(true)
 
   return reply.send({ snapshots, currentArchives })
 }
