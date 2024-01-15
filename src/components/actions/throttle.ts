6c33fec0de7c8b5a0cf123f4aeca445ae3e70082
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2024-01-15 17:19:29 +1300

    Update throttle.ts

--- src/components/actions/throttle.ts
+++ src/components/actions/throttle.ts
@@ -1,20 +1,14 @@
 /**
  * Throttle provides a mechanism to prevent certain functions from being called
- * too many times in rapid succession. Used to limit too many simultaneous
+ * too many times simultaneously. Used to limit too many simultaneous
  * Action Triggers. This can happen (for example) when many thousands of
  * products are set to expire at the exact same time. Without this safeguard,
  * the system is overrun with simultaneous actions which causes numerous errors.
  *
  * The throttle captures all the function calls (using the .add method), but,
  * when multiple calls are made in quick succession, they are put in a queue so
- * they can be executed with appropriate time spacing (using the .runNext
- * method).
+ * they can be executed sequentially.
  */
-
-/**
- * Time between subsequent function calls when queued
- */
-const THROTTLE_SPACING = 1500 // ms
 
 /**
  * Only "add" events captured within this time since the last addition are
@@ -23,7 +17,7 @@ const THROTTLE_SPACING = 1500 // ms
  * experience trigger timeouts due to their event being put at the back of the
  * queue.
  */
-const THROTTLE_QUEUE_THRESHOLD = 500 // ms
+const THROTTLE_QUEUE_THRESHOLD = 200 // ms
 
 type EventObject<T, U> = { name: string; data: T; action: (data: T) => Promise<U> | U }
 
@@ -31,52 +25,44 @@ export class EventThrottle<T, U> {
   queue: EventObject<T, U>[]
   timerId: NodeJS.Timeout | undefined
   lastAdded: number
+  queueActive: boolean
 
   constructor() {
     this.queue = []
     this.lastAdded = 0
+    this.queueActive = false
   }
 
-  runNext = () => {
-    const next = this.queue.shift()
-    if (next) {
-      const { data, action, name } = next
-      console.log('Processing queued event', name)
-      action(data)
-      console.log(`${this.queue.length} events remaining in queue`)
-    }
+  handleQueue = async () => {
+    if (this.queueActive) return
 
-    // Keep looping over this method as long as there are items in the queue
-    if (this.queue.length > 0) {
-      if (this.timerId) clearTimeout(this.timerId)
-      this.timerId = undefined
-      this.timerId = setTimeout(this.runNext, THROTTLE_SPACING)
-    } else if (this.timerId) {
-      console.log('Throttle queue empty')
-      clearTimeout(this.timerId)
-      this.timerId = undefined
+    this.queueActive = true
+    while (this.queue.length > 0) {
+      const next = this.queue.shift()
+      if (next) {
+        const { data, action, name } = next
+        console.log('Processing queued event', name)
+        await action(data)
+        console.log(`${this.queue.length} events remaining in queue`)
+      }
     }
+    this.queueActive = false
   }
 
   public add(event: EventObject<T, U>): void {
-    if (!this.timerId) {
-      // No timer means nothing is waiting, so we run immediately, and start a
-      // timer to determine when additional events are allowed to run
-      const { data, action, name } = event
-      console.log('Processing:', name)
-      action(data)
-      this.timerId = setTimeout(this.runNext, THROTTLE_SPACING)
-      this.lastAdded = Date.now()
-    } else if (Date.now() - this.lastAdded > THROTTLE_QUEUE_THRESHOLD) {
-      // We can run action immediately if THROTTLE_QUEUE_THRESHOLD has elapsed
-      // since the last .add()
-      const { data, action, name } = event
-      console.log('Processing:', name)
-      action(data)
-      this.lastAdded = Date.now()
+    // Timer is used to determine whether to put new events at the front or end of the queue.
+    const currentTimer = this.timerId
+    this.timerId = setTimeout(() => (this.timerId = undefined), THROTTLE_QUEUE_THRESHOLD)
+
+    if (!currentTimer) {
+      console.log('Bumping event to front of queue:', event.name)
+      this.queue.unshift(event)
     } else {
-      console.log('Queuing: ', event.name)
+      console.log('Queueing event:', event.name)
       this.queue.push(event)
+      clearTimeout(currentTimer)
     }
+
+    this.handleQueue()
   }
 }
