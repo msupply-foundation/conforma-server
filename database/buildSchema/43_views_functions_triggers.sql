8baf88e898bd034e4925646587da8db7657b82da
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2024-05-28 11:48:26 +1200

    Re-instate application_list_shape dummy table

--- database/buildSchema/43_views_functions_triggers.sql
+++ database/buildSchema/43_views_functions_triggers.sql
@@ -1866,6 +1866,29 @@ CREATE TRIGGER update_reviewer_stats_from_status
     FOR EACH ROW
     EXECUTE FUNCTION public.update_reviewer_stats_from_status ();
 
+-- This is a dummy table for the application list. Needs to be defined so that
+-- GraphQL types for the list get exposed by Postgraphile
+CREATE TABLE IF NOT EXISTS application_list_shape (
+    id int,
+    "serial" varchar,
+    "name" varchar,
+    template_code varchar,
+    template_name varchar,
+    applicant varchar,
+    org_name varchar,
+    stage varchar,
+    stage_colour varchar,
+    "status" public.application_status,
+    outcome public.application_outcome,
+    last_active_date timestamptz,
+    applicant_deadline timestamptz,
+    -- TO-DO: reviewer_deadline
+    assigners varchar[],
+    reviewers varchar[],
+    reviewer_action public.reviewer_action,
+    assigner_action public.assigner_action
+);
+
 -- APPLICATION_LIST_VIEW
 -- Aggregated VIEW method of all data required for application list page
 -- Requires an empty table as setof return and smart comment to make orderBy work (https://github.com/graphile/graphile-engine/pull/378)
