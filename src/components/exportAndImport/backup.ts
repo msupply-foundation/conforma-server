04acae66a19c825d2465cc64702420fa3e6dec82
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2024-10-29 13:21:53 +1300

    Dont' throw on backup error

--- src/components/exportAndImport/backup.ts
+++ src/components/exportAndImport/backup.ts
@@ -108,13 +108,16 @@ const createBackup = async (password?: string) => {
   } else console.log('No new archives to back up')
 
   // Make new snapshot with no archives
-  const { snapshot } = await takeSnapshot({
+  const { snapshot, error } = await takeSnapshot({
     snapshotName,
     optionsName: 'backup',
     extraOptions: { archive: 'none' },
   })
 
-  if (!snapshot) throw new Error("Couldn't create snapshot")
+  if (!snapshot || error) {
+    console.log('ERROR CREATING SNAPSHOT: ' + error)
+    return
+  }
 
   zipSources.push(snapshot)
 
