00f47a054d1687963aa769d3385eeb8569be33ee
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2024-11-21 13:20:38 +1300

    Tidy up

--- docker/database.sh
+++ docker/database.sh
@@ -8,24 +8,17 @@ cd /usr/src/conforma-server
 echo '--- ADDING SCHEMA'
 ./database/initialise_database.sh tmf_app_manager
 
-# Loading a snapshot from here puts these folders in the repo root rather than
-# the "build" folder so, need to move them in:
-rm -r build/files
-rm -r build/preferences
-rm -r build/localisation
-mv files build
-mv preferences build
-mv localisation build
-
-echo '--- STARTING SERVER'
-node ./build/src/server.js &
-sleep 3
-
 echo '--- ADDING DATA'
 ./database/insert_data.sh $1
 
-echo '--- RUNNING POST INSTALL'
-./database/post_data_insert.sh
+# Loading a snapshot from here puts these folders in the repo root rather than
+# the "build" folder so, need to move them in:
+rm -r -f build/files
+rm -r -f build/preferences
+rm -r -f build/localisation
+mv files build
+mv preferences build
+mv localisation build
 
 echo '--- COPY CLEAN DATABASE TO BE USED IF NO VOLUMES ARE MOUNTED'
 cp -R /var/lib/postgresql/16/main/ ./fresh_db
